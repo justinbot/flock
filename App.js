1e4ef525bc905513f024144553b4a402669ba3c1
Justin Carlson <jrcarlson8@gmail.com>
2018-10-03 21:55:53 -0400

    Add asset preloading, initial Firebase integration.

--- App.js
+++ App.js
@@ -1,32 +1,56 @@
-import React from "react";
+import React from 'react';
 
-import { AppLoading, Asset, Font, Icon } from "expo";
-import { Platform, StatusBar, View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
+import { AppLoading, Asset, Constants, Font, Icon } from 'expo';
+import { Feather } from '@expo/vector-icons';
+import { Platform, StatusBar, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import * as firebase from 'firebase';
 
-import theme from "Constants/Theme";
-import AppNavigator from "Navigation/AppNavigator";
+import theme from 'Constants/Theme';
+import AppNavigator from 'Navigation/AppNavigator';
 
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
 
 export default class App extends React.Component {
   state = {
-    isLoadingComplete: false
+    isReady: false
   };
 
+  componentWillMount() {
+    // Initialize Firebase using configuration info
+    firebase.initializeApp(Constants.manifest.extra.firebaseConfig);
+  }
+
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+    if (!this.state.isReady && !this.props.skipLoadingScreen) {
+      // Display loading screen
+      // See: https://docs.expo.io/versions/latest/sdk/app-loading.html
       return (
         <AppLoading
-          startAsync={this._loadResourcesAsync}
+          startAsync={this._loadAssetsAsync}
           onError={this._handleLoadingError}
           onFinish={this._handleFinishLoading}
         />
       );
     } else {
+      // Display app
       return (
         <PaperProvider theme={theme}>
           <View style={{ flex: 1 }}>
-            {Platform.OS === "ios" && <StatusBar barStyle="default"/>}
+            {Platform.OS === 'ios' && <StatusBar barStyle='default'/>}
             <AppNavigator/>
           </View>
         </PaperProvider>
@@ -34,21 +58,26 @@ export default class App extends React.Component {
     }
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([]),
-      Font.loadAsync({
-        ...Icon.Feather.font
-      })
+  async _loadAssetsAsync() {
+    // Preload assets
+    // See: https://docs.expo.io/versions/latest/guides/preloading-and-caching-assets.html
+
+    const imageAssets = cacheImages([
     ]);
-  };
+
+    const fontAssets = cacheFonts([
+      Feather.font,
+    ]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }
 
   _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error reporting service, for example Sentry
+    // TODO: Log to error reporting
     console.warn(error);
   };
 
   _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
+    this.setState({ isReady: true });
   };
 }
