0e963b3b797072c5e76b75e4daf5bae1bea6ce31
Justin Carlson <jrcarlson8@gmail.com>
2018-10-11 00:16:32 -0400

    Modify build settings to match expo-native-firebase.

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -14,7 +14,7 @@ public class AppConstants {
   public static final String VERSION_NAME = "2.8.1";
   public static String INITIAL_URL = "exp://exp.host/@justinbot/flock";
   public static final boolean IS_DETACHED = true;
-  public static final String SHELL_APP_SCHEME = "expa8eedd51cb0e43ada10f2f6bb4e64c72";
+  public static final String SHELL_APP_SCHEME = "flock";
   public static final String RELEASE_CHANNEL = "default";
   public static boolean SHOW_LOADING_VIEW_IN_SHELL_APP = false;
   public static boolean ARE_REMOTE_UPDATES_ENABLED = true;
@@ -25,9 +25,12 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@justinbot/flock/index.exp", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40justinbot%2Fflock%2F1.0.0%2Ff3e0430ae4f94b4f4d8f0121a2fdc939-30.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
