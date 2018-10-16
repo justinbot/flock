d026785c82e68bb81c8cfeb157db0f90ce90ff03
Justin Carlson <jrcarlson8@gmail.com>
2018-10-16 17:20:17 -0400

    Improve nearby user list layout.

--- src/components/Around/UserList.js
+++ src/components/Around/UserList.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Easing, FlatList, View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { Icon } from 'expo';
-import { Card, Headline, Title } from 'react-native-paper';
 import * as Animatable from 'react-native-animatable';
 
 import theme from 'src/constants/Theme';
@@ -33,7 +32,8 @@ export default class UserList extends React.Component {
             animation="rotate"
             duration={1000}
             easing="ease-out-back"
-            iterationCount="infinite">
+            iterationCount="infinite"
+            useNativeDriver>
             <Icon.Feather name="compass" size={60} color={theme.colors.disabled} />
           </Animatable.View>
         </View>
@@ -43,10 +43,11 @@ export default class UserList extends React.Component {
         <View style={{ justifyContent: 'center', alignItems: 'center' }}>
           <Animatable.View
             animation="tada"
-            duration={1600}
+            duration={1800}
             easing="linear"
             iterationCount="infinite"
-            iterationDelay={400}>
+            iterationDelay={800}
+            useNativeDriver>
             <Icon.Feather name="radio" size={60} color={theme.colors.disabled} />
           </Animatable.View>
         </View>
@@ -60,11 +61,6 @@ export default class UserList extends React.Component {
         data={this.props.data}
         keyExtractor={this._keyExtractor}
         renderItem={this._renderItem}
-        ListEmptyComponent={() => (
-          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Title style={{ color: theme.colors.disabled }}>Looking around...</Title>
-          </View>
-        )}
         ListFooterComponent={this._listFooterComponent}
       />
     );
