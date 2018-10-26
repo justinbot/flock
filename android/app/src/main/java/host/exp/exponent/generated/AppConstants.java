c79533388faecf1b5fa09550cff347d0b85ccae4
Justin Carlson <jrcarlson8@gmail.com>
2018-10-26 16:41:41 -0400

    Update launcher icon, splash, app.json for release build.

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -26,10 +26,11 @@ public class AppConstants {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
     
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@justinbot/flock/index.exp", "assets://shell-app-manifest.json", "application/json"));
-        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40justinbot%2Fflock%2F1.0.0%2Ff3e0430ae4f94b4f4d8f0121a2fdc939-30.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40justinbot%2Fflock%2F1.0.0%2Fe859b5284ae7a5a9cd1fa733aa6e08c5-30.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
         // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
